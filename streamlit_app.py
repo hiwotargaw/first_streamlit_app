5271915a180eb7c40f5ea30f9a22bd84fcbb89bf
Hiwot Argaw <111537128+hiwotargaw@users.noreply.github.com>
2022-08-18 16:51:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit(Omega 3 & Blueberry Oatmeal
+          Kale, Spinach & Rocket Smoothie
+          Hard-Boiled Free Range Egg)
+          
